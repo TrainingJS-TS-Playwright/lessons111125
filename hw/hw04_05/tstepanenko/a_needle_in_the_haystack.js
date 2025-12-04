02e7322df550fefcf51999dbeafe16fb8dfbff40
Taisiia Stepanenko <taisastepanenko55@gmail.com>
2025-12-04 19:15:11 +0200

    hw04_05 completed

---
+++ hw/hw04_05/tstepanenko/a_needle_in_the_haystack.js
@@ -0,0 +1,8 @@
+function findNeedle(haystack) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === "needle") {
+        return "found the needle at position " + i;
+        }
+    }
+  }
+  //console.log(findNeedle(["hay", "junk", "needle", "testjunk"]))
---
+++ hw/hw04_05/tstepanenko/counting_sheep.js
@@ -0,0 +1,10 @@
+function countSheeps(sheep) {
+  let count = 0;
+  for (let i = 0; i < sheep.length; i++) {
+      if (sheep[i] === true) {
+        count = count + 1;
+      }
+    }
+  return count;
+}
+//console.log(countSheeps([true, false, true]))
