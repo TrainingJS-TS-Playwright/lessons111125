abe1f7fca5fbfabdd478b8e682765388579a696c
Daria Shvets <daryyapopovich@gmail.com>
2025-12-12 12:42:22 +0200

    hw 06 Shvets

---
+++ hw/hw06/shvets/class_like.js
@@ -0,0 +1,15 @@
+class Animal {
+constructor (name, type) {
+    this.name = name
+    this.type = type
+}
+toString() {
+   return `${this.name} is a ${this.type}`
+}
+
+}
+
+let dog = new Animal('Max', 'dog');
+dog.name = 'Lassie'
+
+console.log(dog.name)
---
+++ hw/hw06/shvets/color_ghost.js
@@ -0,0 +1,10 @@
+class Ghost {
+    constructor() {
+        const colors = ["white", "yellow", "purple", "red"]
+        this.color = colors[Math.floor(Math.random() * 4)];
+    }
+}
+
+let ghost = new Ghost();
+
+console.log(ghost.color);
